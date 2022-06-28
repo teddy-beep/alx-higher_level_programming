4ae4ff28c042662ab7bd1022044fe4ecdbde1ae2
Tewodros <teddymekuria277@gmail.com>
2022-06-28 06:17:02 -0700

    python if else

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for i in range(10):
     for j in range(10):
         if i < j:
-            print('{:d}{:d}'.format(i, j), end='')
+            print("{:d}{:d}".format(i, j), end="")
             if i < 8:
-                print(', ', end='')
-                print()
+                print(", ", end="")
+                print("")
