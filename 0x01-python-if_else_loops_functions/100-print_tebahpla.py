3567dd577373bc14923c23b18b2fa0fa467cfb20
Tewodros <teddymekuria277@gmail.com>
2022-06-28 07:14:51 -0700

    python if else

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for ch in reversed(range(97, 123):
-        print("{:c}".format(ch if (ch%2 == 0) else (ch -32)), end='')
+for i in reversed(range(97, 123)):
+    if i % 2 == 0:
+        print('{:c}'.format(i), end='')
+    else:
+        print('{:c}'.format(i - 32), end='')
+    
